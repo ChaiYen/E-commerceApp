1baf422bb7e13fc7eaddde6fb48a535e9e42773b
ChaiYen <chyann@huskers.unl.edu>
2022-05-30 02:18:36 -0500

    add sign up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,32 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../components/utils/firebase/firebase.utils";
+
+import { useEffect }from 'react';
+import {getRedirectResult} from 'firebase/auth'
+
+import {
+    auth, 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth, 
+    signInWithGoogleRedirect } 
+    from "../../components/utils/firebase/firebase.utils";
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';    
 
 const SignIn = () =>{
+    useEffect(() =>{
+        getRedirect();
+    }, [])
+
+    const getRedirect = async()=>{
+        const response = await getRedirectResult(auth);
+        if(response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    }
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
-    }    
+    }     
 
     return (
         <div>
@@ -12,6 +34,10 @@ const SignIn = () =>{
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
+            {/* <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button> */}
+            <SignUpForm/>
         </div>
     );
 };
